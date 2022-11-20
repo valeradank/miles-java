9187754dcc959e2b7f1194bccbcece61b95be766
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-11-20 13:05:15 +0300

    another commit

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int moneyInTheAccount = 0; // деньги на счету клиента
-
         int replenishmentAmount = 2000; // сумма пополнения
 
 
         int priceBonus = 10; // сумма бонусный рублей за каждые 100 рублей
 
         int minimumBonusAmount = 1100; // сумма необходимая для получения бонуса
+
+            int moneyInTheAccount = replenishmentAmount / priceBonus + replenishmentAmount; // деньги на счету клиента
         if (replenishmentAmount > minimumBonusAmount) {
-            System.out.println(replenishmentAmount / priceBonus + replenishmentAmount);
-        } else {
+            System.out.println(moneyInTheAccount );
+            int bonus = replenishmentAmount / priceBonus;
+            System.out.println(bonus);
+
+
+        }
+        else {
             System.out.println("недостаточно средств для бонуса");
 
         }
